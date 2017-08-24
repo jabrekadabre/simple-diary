ac7efb429120c1259cc2843322f716c554647cb9
jabrekadabre <jakob.kreft.jk@gmail.com>
2017-08-24 19:32:03 +0200

    Add files via upload

---
+++ clog.py
@@ -0,0 +1,15 @@
+# daily blog
+from time import gmtime, strftime
+
+
+csvfile = "clog.csv"
+def dopisivscv(line,imecsv): #to zapose v csv file nov vspeh
+	with open(imecsv,'a') as f:
+	    f.write(line)
+	    f.write('\n')
+while True:
+	cas = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+	print "wellcome back! write few words about today...:\n (category;santance)"
+	stavek = raw_input("type here: ")
+	stavek = cas + ';' + stavek
+	dopisivscv(stavek,csvfile)
